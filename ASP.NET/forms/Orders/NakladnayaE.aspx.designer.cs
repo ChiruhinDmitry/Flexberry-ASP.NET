3e65afdf1165025a4e967d1008ef855eb9b12554
Chiruhin Dmitriy <dchiruhin@ics.perm.ru>
2021-07-08 16:38:19 +0500

    Выполненный практикум

---
+++ ASP.NET/forms/Orders/NakladnayaE.aspx.designer.cs
@@ -0,0 +1,249 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace IIS.Практикум {
+    
+    
+    public partial class НакладнаяE {
+        
+        /// <summary>
+        /// ctrlСтатусLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlСтатусLabel;
+        
+        /// <summary>
+        /// ctrlСтатус элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ctrlСтатус;
+        
+        /// <summary>
+        /// ctrlДатаВремяОтгрузкиLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlДатаВремяОтгрузкиLabel;
+        
+        /// <summary>
+        /// ctrlДатаВремяОтгрузки элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePicker ctrlДатаВремяОтгрузки;
+        
+        /// <summary>
+        /// ctrlДатаВремяОтгрузкиDatePickerValidator элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePickerValidator ctrlДатаВремяОтгрузкиDatePickerValidator;
+        
+        /// <summary>
+        /// ctrlСуммаLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlСуммаLabel;
+        
+        /// <summary>
+        /// ctrlСумма элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DecimalTextBox ctrlСумма;
+        
+        /// <summary>
+        /// ctrlВесLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlВесLabel;
+        
+        /// <summary>
+        /// ctrlВес элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DecimalTextBox ctrlВес;
+        
+        /// <summary>
+        /// ctrlПримечаниеLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlПримечаниеLabel;
+        
+        /// <summary>
+        /// ctrlПримечание элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ctrlПримечание;
+        
+        /// <summary>
+        /// ctrlФИОПолучателяLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlФИОПолучателяLabel;
+        
+        /// <summary>
+        /// ctrlФИОПолучателя элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ctrlФИОПолучателя;
+        
+        /// <summary>
+        /// ctrlНомерLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlНомерLabel;
+        
+        /// <summary>
+        /// ctrlНомер элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.AlphaNumericTextBox ctrlНомер;
+        
+        /// <summary>
+        /// ctrlДатаЗаполненияLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlДатаЗаполненияLabel;
+        
+        /// <summary>
+        /// ctrlДатаЗаполнения элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePicker ctrlДатаЗаполнения;
+        
+        /// <summary>
+        /// ctrlДатаЗаполненияDatePickerValidator элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePickerValidator ctrlДатаЗаполненияDatePickerValidator;
+        
+        /// <summary>
+        /// ctrlМатериальноОтветственноеЛицоLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlМатериальноОтветственноеЛицоLabel;
+        
+        /// <summary>
+        /// ctrlМатериальноОтветственноеЛицо элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.MasterEditorAjaxLookUp ctrlМатериальноОтветственноеЛицо;
+        
+        /// <summary>
+        /// ctrlМатериальноОтветственноеЛицоRequiredFieldValidator элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator ctrlМатериальноОтветственноеЛицоRequiredFieldValidator;
+        
+        /// <summary>
+        /// ctrlЗаказLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlЗаказLabel;
+        
+        /// <summary>
+        /// ctrlЗаказ элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.MasterEditorAjaxLookUp ctrlЗаказ;
+        
+        /// <summary>
+        /// ctrlЗаказRequiredFieldValidator элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator ctrlЗаказRequiredFieldValidator;
+        
+        /// <summary>
+        /// ScriptManager1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        
+        /// <summary>
+        /// ctrlЗаписьВНакладной элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.AjaxGroupEdit ctrlЗаписьВНакладной;
+    }
+}
---
+++ ASP.NET/forms/Orders/ZakazE.aspx.designer.cs
@@ -0,0 +1,195 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace IIS.Практикум {
+    
+    
+    public partial class ЗаказE {
+        
+        /// <summary>
+        /// ctrlСтатусLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlСтатусLabel;
+        
+        /// <summary>
+        /// ctrlСтатус элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ctrlСтатус;
+        
+        /// <summary>
+        /// ctrlДатаОтгрузкиLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlДатаОтгрузкиLabel;
+        
+        /// <summary>
+        /// ctrlДатаОтгрузки элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePicker ctrlДатаОтгрузки;
+        
+        /// <summary>
+        /// ctrlДатаОтгрузкиDatePickerValidator элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePickerValidator ctrlДатаОтгрузкиDatePickerValidator;
+        
+        /// <summary>
+        /// ctrlДатаОплатыLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlДатаОплатыLabel;
+        
+        /// <summary>
+        /// ctrlДатаОплаты элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePicker ctrlДатаОплаты;
+        
+        /// <summary>
+        /// ctrlДатаОплатыDatePickerValidator элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePickerValidator ctrlДатаОплатыDatePickerValidator;
+        
+        /// <summary>
+        /// ctrlЦенаLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlЦенаLabel;
+        
+        /// <summary>
+        /// ctrlЦена элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DecimalTextBox ctrlЦена;
+        
+        /// <summary>
+        /// ctrlНомерLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlНомерLabel;
+        
+        /// <summary>
+        /// ctrlНомер элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.AlphaNumericTextBox ctrlНомер;
+        
+        /// <summary>
+        /// ctrlДатаЗаполненияLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlДатаЗаполненияLabel;
+        
+        /// <summary>
+        /// ctrlДатаЗаполнения элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePicker ctrlДатаЗаполнения;
+        
+        /// <summary>
+        /// ctrlДатаЗаполненияDatePickerValidator элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.DatePickerValidator ctrlДатаЗаполненияDatePickerValidator;
+        
+        /// <summary>
+        /// ctrlМенеджерLabel элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ctrlМенеджерLabel;
+        
+        /// <summary>
+        /// ctrlМенеджер элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.MasterEditorAjaxLookUp ctrlМенеджер;
+        
+        /// <summary>
+        /// ctrlМенеджерRequiredFieldValidator элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator ctrlМенеджерRequiredFieldValidator;
+        
+        /// <summary>
+        /// ScriptManager1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+        
+        /// <summary>
+        /// ctrlСтрокаЗаказа элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::ICSSoft.STORMNET.Web.AjaxControls.AjaxGroupEdit ctrlСтрокаЗаказа;
+    }
+}
